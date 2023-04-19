a757d9983c2b75e27ce7da49d539a53a91fd2f49
Arthur Teixeira <arthursony1@hotmail.com>
2023-04-19 16:31:16 -0300

    group 4 - requisito 2

---
+++ src/pages/Home.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  state = {
+    search: '',
+    searchList: [],
+  };
+
+  handleChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    }, () => getProductsFromCategoryAndQuery(params));
+  };
+
+  render() {
+    const { search, searchList } = this.state;
+    return (
+      <div>
+        <input type="text" name="search" id="search" onChange={ this.handleChange } />
+        <div>
+          <p data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </p>
+          {/* { search !== ''
+            ? (
+              <p data-testid="home-initial-message">
+                Digite algum termo de pesquisa ou escolha uma categoria.
+              </p>)
+            : searchList
+              .map((element, index) => (<Item
+                key={ index }
+                id={ element.id }
+                name={ element.title }
+                price={ element.price }
+                shipping={ element.shipping.free_shipping }
+              />)) } */}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
